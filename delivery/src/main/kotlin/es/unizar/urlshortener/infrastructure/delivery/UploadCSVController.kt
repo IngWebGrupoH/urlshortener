afb9bf74ac1d7221dc0a6a9aea6f47261e322562
aechavarris <aechavarris4@gmail.com>
2021-11-11 00:27:32 +0100

    CSV recibidos y tratables en controller

--- delivery/src/main/kotlin/es/unizar/urlshortener/infrastructure/delivery/UploadCSVController.kt
+++ delivery/src/main/kotlin/es/unizar/urlshortener/infrastructure/delivery/UploadCSVController.kt
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
+
+import es.unizar.urlshortener.core.usecases.LogClickUseCase
+import es.unizar.urlshortener.core.usecases.CreateShortUrlUseCase
+import es.unizar.urlshortener.core.usecases.RedirectUseCase
 import java.io.BufferedReader
 
 
@@ -17,16 +21,20 @@ interface UploadCSVController {
     fun handleFileUpload(@RequestParam("file") file: MultipartFile): CSVResponse
 }
 @RestController
-class UploadCSVControllerImpl {
+class UploadCSVControllerImpl(
+    val redirectUseCase: RedirectUseCase,
+    val logClickUseCase: LogClickUseCase,
+    val createShortUrlUseCase: CreateShortUrlUseCase
+    ):UploadCSVController {
+    val logger = LoggerFactory.getLogger(UploadCSVControllerImpl::class.java)
 
-    val logger = LoggerFactory.getLogger(UploadCSVController::class.java)
-
-    @PostMapping("/fileupload")
-    fun handleFileUpload(@RequestParam("file") file: MultipartFile): CSVResponse {
+    @PostMapping("/api/uploadCSV")
+    override fun handleFileUpload(@RequestParam("csv") file: MultipartFile): CSVResponse {
         logger.info("handling fileupload for {}", file.name)
-        val content = file.inputStream.bufferedReader().use(BufferedReader::readText)
-        logger.info("file content = {}", content)
-        return CSVResponse.ok(content)
+        val content = String(file.getBytes());//inputStream.bufferedReader().use(BufferedReader::readText)
+        val urlArray=content.split("\n")
+        logger.info("file content = {}", urlArray)
+        return CSVResponse.ok(urlArray.toString())
     }
 }
 
