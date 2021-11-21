219a087c9dc19bcec60dfd3a6782b6099ca11ad5
aechavarris <aechavarris4@gmail.com>
2021-11-21 20:34:44 +0100

    Ahora llega la request

--- app/src/main/resources/static/js/csv.js
+++ app/src/main/resources/static/js/csv.js
@@ -4,12 +4,14 @@ $(document).ready(
             function(event) {
                 event.preventDefault();
                 var formFile = new FormData();
-                formFile.append('csv',$( '#fileInput' )[0].files[0]);
+                formFile.append('csv',$('#fileInput')[0].files[0]);
                 $.ajax({
                     type : "POST",
                     url : "/api/CSVUpload",
-                    data : formFile,
+                    data : $(this).serialize(),
                     enctype : 'multipart/form-data',
+                    contentType : 'multipart/form-data',
+                    processData: false,
                     success : function(msg, status, request) {
                         $("#result").html(
                             "<div class='alert alert-success lead'><a target='_blank' href='"
