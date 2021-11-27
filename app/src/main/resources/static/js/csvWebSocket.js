92224e68d622fbc1dd83907b7199301cae464852
aechavarris <aechavarris4@gmail.com>
2021-11-27 23:34:12 +0100

    Websockets funciona

--- app/src/main/resources/static/js/csvWebSocket.js
+++ app/src/main/resources/static/js/csvWebSocket.js
@@ -1,21 +1,24 @@
-//var webSocket = new WebSocket("ws://" + location.hostname + ":" + location.port + "/chat");
-var webSocket = SockJS('/api');
-
-webSocket.onmessage = function (msg) { receievMsg(JSON.parse(msg.data)) }
-webSocket.onclose = function() { alert("Server Disconnect You"); location.reload() }
-$(document).ready(
-    $("#fileInputForm").submit(function () {
-        var formFile = new FormData();
-        formFile.append('csv',$('#fileInput')[0].files[0]);
-        sendMessage("uploadCSV", formFile);
-        window.alert($('#fileInput')[0].files[0].name)
-    })
-);
-function sendMessage(type, data) {
-    if (data !== "") {
-        webSocket.send(JSON.stringify({type: type, data: data}));
-    }
+//var webSocket = SockJS('/');
+function connect() {
+    
 }
-function receievMsg(msg) {
-    $("#CSVResult").append("<p>"+msg.data+"</p>");
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+function send() {
+    ws = new WebSocket("ws://localhost:8080/websocket/CSVUpload");
+    ws.onclose = function() { alert("Server Disconnect You"); location.reload() }
+    ws.onmessage = function(event) {
+      $("#result").append("<p>"+event.data+"</p>");
+
+    };
+    sleep(500).then(() => {
+        var reader = new FileReader();
+        reader.onload = function(event) {
+            console.log('File content:', event.target.result);
+            ws.send(event.target.result);
+        };
+        reader.readAsText($('#fileInput')[0].files[0]);
+    });
+    
 }
--- delivery/src/main/kotlin/es/unizar/urlshortener/infrastructure/delivery/UploadCSVWebSocketController.kt
+++ delivery/src/main/kotlin/es/unizar/urlshortener/infrastructure/delivery/UploadCSVWebSocketController.kt
@@ -1,60 +1,103 @@
 package es.unizar.urlshortener.infrastructure.delivery
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.socket.*
-import org.springframework.web.socket.config.annotation.*
-import org.springframework.web.socket.handler.AbstractWebSocketHandler
+import org.springframework.stereotype.Component
+import org.springframework.web.socket.CloseStatus
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketSession
+import org.springframework.web.socket.config.annotation.EnableWebSocket
+import org.springframework.web.socket.server.standard.ServerEndpointExporter
+import java.util.*
+import javax.websocket.*
+import javax.websocket.CloseReason.CloseCodes
+import javax.websocket.server.ServerEndpoint
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import org.springframework.web.multipart.commons.CommonsMultipartFile
+import es.unizar.urlshortener.core.*
 
-import java.util.concurrent.atomic.AtomicLong
-import org.slf4j.LoggerFactory
+import es.unizar.urlshortener.core.usecases.LogClickUseCase
+import es.unizar.urlshortener.core.usecases.RedirectUseCase
+import es.unizar.urlshortener.core.usecases.CreateShortUrlUseCase
+import es.unizar.urlshortener.core.ShortUrlProperties
+import es.unizar.urlshortener.core.usecases.*
 
-class User(val id: Long, val name: String)
-class Message(val msgType: String, val data: Any)
+import org.springframework.web.bind.annotation.*
+import org.springframework.hateoas.server.mvc.linkTo
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.web.socket.handler.TextWebSocketHandler
+import java.net.*
+import javax.servlet.http.HttpServletRequest
 
-class UploadCSVWebSocketController : AbstractWebSocketHandler() {
+import java.io.IOException
+import java.io.StringWriter
 
-    val sessionList = HashMap<WebSocketSession, User>()
-    var uids = AtomicLong(0)
+@Component
+public class UploadCSVWebSocketController(
+    val createShortUrl :CreateShortUrlUseCase
+): TextWebSocketHandler(){
 
     @Throws(Exception::class)
+    override fun afterConnectionEstablished(session: WebSocketSession) { //the messages will be broadcasted to all users.
+        println("received connection")
+    }
+ 
     override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
-        sessionList -= session
+        super.afterConnectionClosed(session, status)
+        println("Connection closed by client")
     }
-    
-    override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
-        val json = ObjectMapper().readTree(message?.payload)
-        val logger = LoggerFactory.getLogger(this.javaClass)
-        logger.info("Message type "+json.get("type").asText()+" received");
-        // {type: "join/say", data: "name/msg"}
-        when (json.get("type").asText()) {
-            // "join" -> {
-            //     val user = User(uids.getAndIncrement(), json.get("data").asText())
-            //     sessionList.put(session!!, user)
-            //     // tell this user about all other users
-            //     emit(session, Message("shorteredCSV", sessionList.values))
-            // }
-            "uploadCSV" -> {
-                val user = User(uids.getAndIncrement(), json.get("data").asText())
-                sessionList.put(session!!, user)
-                logger.info(user.name);
-                
-
-                emit(session, Message("CSVResult", user.name))
-            }
+    /**
+     * Message received
+     *
+     * @param message
+     */
+    @Throws(InterruptedException::class, IOException::class)
+    public override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
+        LOGGER.info("Server Message ... Session "+session.id)
+        LOGGER.info("Server received "+ message.payload)
+        
+        val content = message.payload.split("\n")
+        if(content.isEmpty()){
+            val h = HttpHeaders()
+            h.location = null
+            val response = CSVResponse(
+                data= null,
+                isSuccess = false
+            )
+            session.close(CloseStatus(2, "File empty!"));
         }
+        val shortUrlArray=ArrayList<CSVDataOut>()
+        for (i in content) {
+            val url = URL(i);
+            val nullFragment = null;
+            val uri = URI(url.getProtocol(), url.getHost(), url.getPath(), url.getQuery(), nullFragment);
+            LOGGER.info("url="+uri.toString());
+            val response=createShortUrl.create(uri.toString(), ShortUrlProperties(
+                ip = "request.remoteAddr"
+            )) 
+            shortUrlArray.add(CSVDataOut(uri,ShortUrlDataOut(
+                url = URI(response.hash),
+                properties = mapOf(
+                    "safe" to response.properties.safe
+                )
+            )))
+            session.sendMessage(TextMessage(("http://localhost:8080/tiny-"+URI(response.hash).toString())))
+            Thread.sleep(1_000)
+        }  
+    }
+    @OnError
+    public fun onError(session: Session, errorReason: Throwable) {
+        LOGGER.error("Session ${session.id} closed because of ${errorReason.javaClass.name}", errorReason)
     }
 
-    fun emit(session: WebSocketSession, msg: Message) = session.sendMessage(TextMessage(jacksonObjectMapper().writeValueAsString(msg)))
-    fun broadcast(msg: Message) = sessionList.forEach { emit(it.key, msg) }
-    fun broadcastToOthers(me: WebSocketSession, msg: Message) = sessionList.filterNot { it.key == me }.forEach { emit(it.key, msg) }
-}
-
-@Configuration @EnableWebSocket
-class WSConfig : WebSocketConfigurer {
-    override fun registerWebSocketHandlers(registry: WebSocketHandlerRegistry) {
-        registry.addHandler(UploadCSVWebSocketController(), "/api").withSockJS()
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(this::class.java)
     }
 }
