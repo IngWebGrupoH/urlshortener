04c9138a0c8d780502a3942d68bb8d00adea2506
SergioGCH <721520@unizar.es>
2021-12-01 13:50:11 +0100

    Generador de qr con checkbox

--- app/src/main/resources/static/js/qr.js
+++ app/src/main/resources/static/js/qr.js
@@ -1,20 +1,26 @@
 $(document).ready(
     function() {
-        $("#qrForm").submit(
+        $("#shortener").submit(
             function(event) {
                 event.preventDefault();
-                $.ajax({
-                    type : "GET",
-                    url : "/api/URLToQR",
-                    data : $(this).serialize(),
-                    success : function(image, request) {
-                        $("#result").html(
-                            "<img id=ItemPreview src=data:image/png;base64,"+image+">");
-                    },
-                    error : function(e) {
-                        $("#result").html(
-                            "<div class='alert alert-danger lead'>ERROR</div>");
-                    }
-                });
+                if(($('#bc').is(':checked'))) {
+                    $.ajax({
+                        type : "GET",
+                        url : "/api/URLToQR",
+                        data : $(this).serialize(),
+                        success : function(image, request) {
+                            $("#resultQr").html(
+                                "<img id=ItemPreview src=data:image/png;base64,"+image+">");
+                        },
+                        error : function(e) {
+                            $("#resultQr").html(
+                                "<div class='alert alert-danger lead'>ERROR</div>");
+                        }
+                    });
+                }else{
+                    $("#resultQr").html(
+                        "");
+                    
+                }
             });
     });
